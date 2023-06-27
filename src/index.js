3e37055935e30c7d48d3c5dc05a9e117b1651050
montree <montri3112545@gmail.com>
2023-06-27 15:22:58 +0700

    new app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+
+function App() {
+  
+  return (
+    <>
+      <div className='card'>
+        <Avatar/>
+        <div className='data'>
+        <Intro/>
+        <Skilllist/>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Avatar() {
+  return <img className='avatar' src='small.jpg' alt='My Avatar'/>;
+}
+
+function Skilllist(){
+  return(
+    <div className='skill-list'>
+      <Skill skill="Mysql" emoji="😨" color="cyan"/>
+      <Skill skill="Html" emoji="😴" color="orange"/>
+      <Skill skill="Css" emoji="🤮" color="yellow"/>
+      <Skill skill="React" emoji="😱" color="orangered"/>
+    </div>
+  );
+}
+
+function Skill(props){
+  return(
+    <div className='skill' style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>นายมนตรี ชะนวนกลาง</h1>
+      <p>
+        นักศึกษา คณะวิทยาศาสตร์ สาขาเทคโนโลยีสารสนเทศเพื่อการสื่อสาร
+        มหาวิทยาลัยอุบลราชธานี งานอดิเรกชอบเล่นเกม อ่านการ์ตูน นอน
+      </p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +60,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
